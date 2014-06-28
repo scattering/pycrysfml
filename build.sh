b071eba58c8fc7d956239f00241db4bb40d4aacf
Joseph E. Lesniewski <joseph.lesniewski@nist.gov>
2014-06-28 19:30:51 -0400

    Added MacOS support to build script. Added method to cfml_python.f90

--- build.sh
+++ build.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 wd=$(pwd)
+# Mac OS Support
+if [[ "$OSTYPE" == "darwin"* ]]; then
+CPPCOMP=g++-4.8
+SEDCOM=gsed
+LIBFLAGS='-lpython -lgfortran'
+SOFLAGS='-shared -fPIC'
+else
+CPPCOMP=g++
+SEDCOM=sed
+LIBFLAGS='-lgfortran'
+SOFLAGS='-shared -fPIC -rdynamic'
+fi
 svn co http://forge.epn-campus.eu/svn/crysfml/Src
+cp $wd/fort_methods/cfml_python/cfml_python.f90 $wd/Src/cfml_python.f90
 # inject fortran methods
 cd $wd/fort_methods
 ./cat.sh
@@ -17,13 +30,14 @@ cd $wd
 $wd/gen_list.py > $wd/list
 cd $wd/Src/
 mkdir wrap
-sed -i 's/.*/\L&/' *.f90
+$SEDCOM -i 's/.*/\L&/' *.f90
 $wd/fix_deps.py
 $wd/fix_type_decl.py
 # wrap library
 $wd/fortwrap.py --file-list=$wd/list -d wrap >& $wd/FortWrap_log
 svn co http://forge.epn-campus.eu/svn/crysfml/Src
 cp Src/*.f90 .
+cp $wd/fort_methods/cfml_python/cfml_python.f90 $wd/Src/cfml_python.f90
 rm -r Src
 # inject fortran methods again and fix line length
 cd $wd/fort_methods
@@ -73,10 +87,14 @@ gfortran -fPIC -o CppWrappers.o -c CppWrappers.f90 ../crysfml.so -lstdc++ -Xlink
 # fix circular dependencies
 cp $wd/fix_circular/* .
 #compile c++
-g++ -O2 -fPIC -c *.cpp *.cxx ../crysfml.so -I/usr/include/python2.7 -lgfortran -Xlinker -rpath .
+#g++ -O2 -fPIC -c *.cpp *.cxx ../crysfml.so -I/usr/include/python2.7 -lgfortran -Xlinker -rpath .
+#fixed for Mac:
+#g++-4.8 -O2 -fPIC -c *.cpp *.cxx ../crysfml.so -I/usr/include/python2.7 -lpython -lgfortran -Xlinker -rpath .
+$CPPCOMP -O2 -fPIC -c *.cpp *.cxx ../crysfml.so -I/usr/include/python2.7 $LIBFLAGS -Xlinker -rpath .
 #build shared-object library
 #g++ -shared -fPIC -rdynamic -o _pycrysfml.so -g -Wall ./*.o ../CFML_GlobalDeps_Linux.o ../CFML_Math_Gen.o ../CFML_LSQ_TypeDef.o ../CFML_Spher_Harm.o ../CFML_Random.o ../CFML_FFTs.o ../CFML_String_Util.o ../CFML_IO_Mess.o ../CFML_Profile_TOF.o ../CFML_Profile_Finger.o ../CFML_Profile_Functs.o ../CFML_Math_3D.o ../CFML_Optimization.o ../CFML_Optimization_LSQ.o ../CFML_Sym_Table.o ../CFML_Chem_Scatt.o ../CFML_Diffpatt.o ../CFML_Bonds_Table.o ../CFML_Cryst_Types.o ../CFML_Symmetry.o ../CFML_ILL_Instrm_Data.o ../CFML_EoS_Mod.o ../CFML_Reflct_Util.o ../CFML_Atom_Mod.o ../CFML_Export_Vtk.o ../CFML_Sfac.o ../CFML_Geom_Calc.o ../CFML_Propagk.o ../CFML_Maps.o ../CFML_Molecules.o ../CFML_SXTAL_Geom.o ../CFML_Conf_Calc.o ../CFML_Form_CIF.o ../CFML_Optimization_SAn.o ../CFML_MagSymm.o ../CFML_Msfac.o ../CFML_Polar.o ../CFML_Refcodes.o -lgfortran -I.
-g++ -shared -fPIC -rdynamic -o _pycrysfml.so -g -Wall ./*.o ../CFML_GlobalDeps_Linux.o ../CFML_Math_Gen.o ../CFML_LSQ_TypeDef.o ../CFML_Spher_Harm.o ../CFML_Random.o ../CFML_FFTs.o ../CFML_String_Util.o ../CFML_IO_Mess.o ../CFML_Profile_TOF.o ../CFML_Profile_Finger.o ../CFML_Profile_Functs.o ../CFML_Math_3D.o ../CFML_Optimization.o ../CFML_Optimization_LSQ.o ../CFML_Sym_Table.o ../CFML_Chem_Scatt.o ../CFML_Diffpatt.o ../CFML_Bonds_Table.o ../CFML_Cryst_Types.o ../CFML_Symmetry.o ../CFML_ILL_Instrm_Data.o ../CFML_Reflct_Util.o ../CFML_Atom_Mod.o ../CFML_Export_Vtk.o ../CFML_Sfac.o ../CFML_Geom_Calc.o ../CFML_Propagk.o ../CFML_Maps.o ../CFML_Molecules.o ../CFML_SXTAL_Geom.o ../CFML_Conf_Calc.o ../CFML_Form_CIF.o ../CFML_MagSymm.o ../CFML_Msfac.o ../CFML_Polar.o ../CFML_Refcodes.o -lgfortran
+$CPPCOMP $SOFLAGS -o _pycrysfml.so -g -Wall ./*.o ../CFML_GlobalDeps_Linux.o ../CFML_Math_Gen.o ../CFML_LSQ_TypeDef.o ../CFML_Spher_Harm.o ../CFML_Random.o ../CFML_FFTs.o ../CFML_String_Util.o ../CFML_IO_Mess.o ../CFML_Profile_TOF.o ../CFML_Profile_Finger.o ../CFML_Profile_Functs.o ../CFML_Math_3D.o ../CFML_Optimization.o ../CFML_Optimization_LSQ.o ../CFML_Sym_Table.o ../CFML_Chem_Scatt.o ../CFML_Diffpatt.o ../CFML_Bonds_Table.o ../CFML_Cryst_Types.o ../CFML_Symmetry.o ../CFML_ILL_Instrm_Data.o ../CFML_Reflct_Util.o ../CFML_Atom_Mod.o ../CFML_Export_Vtk.o ../CFML_Sfac.o ../CFML_Geom_Calc.o ../CFML_Propagk.o ../CFML_Maps.o ../CFML_Molecules.o ../CFML_SXTAL_Geom.o ../CFML_Conf_Calc.o ../CFML_Form_CIF.o ../CFML_MagSymm.o ../CFML_Msfac.o ../CFML_Polar.o ../CFML_Refcodes.o ../cfml_python.o $LIBFLAGS
+#$CPPCOMP -shared -fPIC -o _pycrysfml.so -g -Wall ./*.o ../crysfml.so $LIBFLAGS -L.. -I.. -Xlinker -rpath .
 cd $wd
 make clean
 # install to hklgen directory
--- fort_methods/cfml_python/cfml_python.f90
+++ fort_methods/cfml_python/cfml_python.f90
@@ -1,4 +1,20 @@
 Module cfml_python
 USE CFML_IO_Formats
-	CONTAINS
+Use CFML_Crystal_Metrics,           only: Crystal_Cell_Type
+Use CFML_Crystallographic_Symmetry, only: Space_Group_Type
+Use CFML_Atom_TypeDef,              only: atom_list_type
+CONTAINS
+SUBROUTINE ReadXtal_Structure_File(filenam, Cell, SpG, A, Mode, Iphase, Job_Info, file_list, CFrame)
+!---- Arguments ----!
+       character(len=*), intent(in) :: filenam
+       Type (Crystal_Cell_Type), intent(out) :: Cell
+       Type (Space_Group_Type), intent(out) :: SpG
+       Type (atom_list_type), intent(out) :: A
+       Character(len=*), optional, intent(in) :: Mode
+       Integer, optional, intent(in) :: Iphase
+       Type(Job_Info_type), optional, intent(out) :: Job_Info
+       Type(file_list_type), optional, intent(in out) :: file_list
+       Character(len=*), optional, intent(in) :: CFrame
+       call Readn_Set_Xtal_Structure(filenam, Cell, SpG, A, Mode, Iphase, Job_Info, file_list, CFrame)
+END SUBROUTINE ReadXtal_Structure_File
 End Module cfml_python
