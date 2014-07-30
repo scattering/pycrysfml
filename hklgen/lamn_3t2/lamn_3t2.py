c0fda51a4dd18ba1ba20aff0465d13f24fb92a9d
Joseph E. Lesniewski <joseph.lesniewski@nist.gov>
2014-07-30 15:21:37 -0400

    More example updates

--- hklgen/lamn_3t2/lamn_3t2.py
+++ hklgen/lamn_3t2/lamn_3t2.py
@@ -5,24 +5,24 @@ import hkl_model as Mod
 np.seterr(divide="ignore", invalid="ignore")    
 
 DATAPATH = os.path.dirname(os.path.abspath(__file__))
-backgFile = os.path.join(DATAPATH,"ncaf_3t2.bac")
-observedFile = os.path.join(DATAPATH,"ncaf_3t2.dat")
-infoFile = os.path.join(DATAPATH,"ncaf_3t2.pcr")
+backgFile = os.path.join(DATAPATH,"lamn_3t2.bac")
+observedFile = os.path.join(DATAPATH,"lamn_3t2.dat")
+infoFile = os.path.join(DATAPATH,"lamn_3t2.pcr")
 
 (spaceGroup, crystalCell, atoms) = H.readInfo(infoFile)
 wavelength = 1.225300
 backg = H.LinSpline(None)
-ttMin = 0.0
-ttMax = 125.450004578
-ttStep = 0.049980081505
-exclusions = [[0,5],[125,180]]
+exclusions = [[-1.00,11.00],[125.0,180.0]]
 tt, observed = H.readIllData(observedFile, "D1A", backgFile)
+ttMin = min(tt)
+ttMax = max(tt)
+ttStep = (ttMax-ttMin)/len(tt)
 def fit():
     # PYTHONPATH=. bumps Al2O3.py --fit=dream --store=M1 --burn=100 --steps=500
     cell = Mod.makeCell(crystalCell, spaceGroup.xtalSystem())
     cell.a.pm(0.5)
     m = Mod.Model(tt, observed, backg, 0, 0, 1, wavelength, spaceGroup, cell,
-                atoms, exclusions, base=min(observed), zero=-0.09459)
+                atoms, exclusions, base=min(observed), zero=0.00029)
     m.u.range(0,2)
     m.zero.pm(0.1)
     m.v.range(-2,0)
@@ -31,16 +31,16 @@ def fit():
     m.scale.range(0,10)
     m.base.pm(250)
     for atomModel in m.atomListModel.atomModels:
-        atomModel.x.pm(1.0)
-        if H.getAtom_chemsymb(atomModel.atom).lower() != "ca":
+        atomModel.B.range(0,10)
+        if H.getAtom_chemsymb(atomModel.atom).lower() != "mn":
+            atomModel.x.pm(1.0)
             atomModel.y.pm(1.0)
-            atomModel.z.pm(1.0)
         if (atomModel.atom.multip == atomModel.sgmultip):
             # atom lies on a general position
             atomModel.x.pm(0.1)
             atomModel.y.pm(0.1)
             atomModel.z.pm(0.1)
-    #m.atomListModel["Al1"].z.pm(0.1)
+    m.atomListModel["O2"].z.pm(1.0)
     #m.atomListModel["O1"].x.pm(0.1)
     #m.atomListModel["O3"].y.pm(0.1)
     #m.atomListModel["Pb"].B.range(0,10)
@@ -49,10 +49,10 @@ def fit():
     return M
 
 def main():
-    cell = H.CrystalCell([10.250222,10.250222,10.250222],[90,90,90])
-    uvw = [0.270721,-0.425009,0.216537]
+    cell = H.CrystalCell([5.536482,5.747128,7.692561],[90,90,90])
+    uvw = [0.176001,-0.197806,0.091452]
     H.diffPattern(infoFile=infoFile, wavelength=wavelength,
-                  cell=cell, uvw=uvw, scale=0.34815E-02,
+                  cell=cell, uvw=uvw, scale=0.86927E-01,
                   ttMin=ttMin, ttMax=ttMax, info=True, plot=True,
                   observedData=(tt,observed), base=min(observed))
 
--- hklgen/ybacud1a/M1/ybacud1a.py
+++ hklgen/ybacud1a/M1/ybacud1a.py
@@ -5,24 +5,26 @@ import hkl_model as Mod
 np.seterr(divide="ignore", invalid="ignore")    
 
 DATAPATH = os.path.dirname(os.path.abspath(__file__))
-backgFile = os.path.join(DATAPATH,"ncaf_3t2.bac")
-observedFile = os.path.join(DATAPATH,"ncaf_3t2.dat")
-infoFile = os.path.join(DATAPATH,"ncaf_3t2.pcr")
+backgFile = os.path.join(DATAPATH,"ybacud1a.bac")
+observedFile = os.path.join(DATAPATH,"ybacud1a.dat")
+infoFile = os.path.join(DATAPATH,"ybacud1a.pcr")
 
 (spaceGroup, crystalCell, atoms) = H.readInfo(infoFile)
-wavelength = 1.225300
+wavelength = 1.907972
 backg = H.LinSpline(None)
 ttMin = 0.0
-ttMax = 125.450004578
-ttStep = 0.049980081505
-exclusions = [[0,5],[125,180]]
+ttMax = 155.449996948
+ttStep = 0.0499839218483
+exclusions = [[0,6],[155.45,180]]
 tt, observed = H.readIllData(observedFile, "D1A", backgFile)
 def fit():
     # PYTHONPATH=. bumps Al2O3.py --fit=dream --store=M1 --burn=100 --steps=500
     cell = Mod.makeCell(crystalCell, spaceGroup.xtalSystem())
     cell.a.pm(0.5)
+    cell.b.pm(0.5)
+    cell.c.pm(0.5)
     m = Mod.Model(tt, observed, backg, 0, 0, 1, wavelength, spaceGroup, cell,
-                atoms, exclusions, base=min(observed), zero=-0.09459)
+                atoms, exclusions, base=min(observed), zero=0.19089)
     m.u.range(0,2)
     m.zero.pm(0.1)
     m.v.range(-2,0)
@@ -31,16 +33,18 @@ def fit():
     m.scale.range(0,10)
     m.base.pm(250)
     for atomModel in m.atomListModel.atomModels:
-        atomModel.x.pm(1.0)
-        if H.getAtom_chemsymb(atomModel.atom).lower() != "ca":
-            atomModel.y.pm(1.0)
-            atomModel.z.pm(1.0)
+        atomModel.B.range(0,10)
         if (atomModel.atom.multip == atomModel.sgmultip):
             # atom lies on a general position
             atomModel.x.pm(0.1)
             atomModel.y.pm(0.1)
             atomModel.z.pm(0.1)
-    #m.atomListModel["Al1"].z.pm(0.1)
+    m.atomListModel["Ba"].z.pm(1.0)
+    m.atomListModel["CaB"].z.pm(1.0)
+    m.atomListModel["Cu2"].z.pm(1.0)
+    m.atomListModel["O1"].z.pm(1.0)
+    m.atomListModel["O2"].z.pm(1.0)
+    m.atomListModel["O3"].z.pm(1.0)
     #m.atomListModel["O1"].x.pm(0.1)
     #m.atomListModel["O3"].y.pm(0.1)
     #m.atomListModel["Pb"].B.range(0,10)
@@ -49,12 +53,12 @@ def fit():
     return M
 
 def main():
-    cell = H.CrystalCell([10.250222,10.250222,10.250222],[90,90,90])
-    uvw = [0.270721,-0.425009,0.216537]
+    cell = H.CrystalCell([3.815149, 3.880053, 11.664815],[90,90,90])
+    uvw = [0.198607,-0.322513,0.338442]
     H.diffPattern(infoFile=infoFile, wavelength=wavelength,
-                  cell=cell, uvw=uvw, scale=0.34815E-02,
+                  cell=cell, uvw=uvw, scale=2.5189,
                   ttMin=ttMin, ttMax=ttMax, info=True, plot=True,
-                  observedData=(tt,observed), base=min(observed))
+                  observedData=(tt,observed))
 
 if __name__ == "__main__":
     # program run normally
