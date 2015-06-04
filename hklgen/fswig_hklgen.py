Corrected bugs with calculation of magnetic structure factors and added corrected CFL file for Hobk (Hobk1.cfl) TODO: fix magnetic structure calculation when called from bumps fit.

hklgen/fswig_hklgen.py
@@ -539,7 +539,6 @@ def readInfo(filename):
 # Peak: represents a Peak shape function that can be evaluated at any
 #   2*theta value. u, v, and w are fitting parameters.
 class Peak(object):
-    # TODO: add option for psuedo-Voigt and other peak shapes
     scaleFactor = 1    
     def __init__(self, center, u, v, w, I, hkl=[None, None, None], shape="Gaussian", eta=0):
         self.center = center    # 2*theta position
@@ -771,9 +770,11 @@ def hklGen(spaceGroup, cell, sMin, sMax, getList=False, xtal=False):
 def satelliteGen(cell, symmetry, sMax, hkls=None):
     refList = ReflectionList(True)
     if hkls != None:
-        funcs.gen_satellites(cell, symmetry, sMax, refList, int_to_p(1), int_to_p(1), hkls)
+        #funcs.gen_satellites(cell, symmetry, sMax, refList, int_to_p(1), int_to_p(1), hkls)
+        funcs.gen_satellites(cell, symmetry, sMax, refList, None, None, hkls)
     else:
-        funcs.gen_satellites(cell, symmetry, sMax, refList, int_to_p(1), int_to_p(1))
+        #funcs.gen_satellites(cell, symmetry, sMax, refList, int_to_p(1), int_to_p(1))
+        funcs.gen_satellites(cell, symmetry, sMax, refList)
     return refList
 
 # calcStructFact: calculates the structure factor squared for a list of planes
@@ -794,16 +795,14 @@ def calcStructFact(refList, atomList, spaceGroup, wavelength):
 
 # calcMagStructFact: calculates the magnetic structure factors around a list
 #   of lattice reflections
-# TODO: convert to pycrysfml
-def calcMagStructFact(refList, atomList, symmetry, cell):
+def calcMagStructFact(refList, atomList, symmetry, cell): 
     funcs.init_mag_structure_factors(refList, atomList, symmetry)
-    funcs.mag_structure_factors(atomList, symmetry, refList)
+    funcs.mag_structure_factors(cell, atomList, symmetry, refList)
     # calculate the "magnetic interaction vector" (the square of which is
     #   proportional to the intensity)    
     funcs.calc_mag_interaction_vector(refList, cell)
     #for atom in atomList:
         #print atom.basis()
-    
     mivs = np.array([ref.magIntVec() for ref in refList])
     return mivs
 
@@ -896,13 +895,14 @@ def diffPattern(infoFile=None, backgroundFile=None, wavelength=1.5403,
             if (symmetry == None): symmetry = info[3]
         if (basisSymmetry == None): basisSymmetry = symmetry
         ## magnetic peaks
-        magRefList = satelliteGen(cell, symmetry, sMax)
+        refList = hklGen(spaceGroup, cell, sMin, sMax, True, xtal=xtal)
+        magRefList = satelliteGen(cell, symmetry, sMax, hkls=refList)
         print "length of reflection list " + str(len(magRefList))
         magIntensities = calcIntensity(magRefList, magAtomList, basisSymmetry,
                                        wavelength, cell, True)
         # add in structural peaks
         if (atomList == None): atomList = readInfo(infoFile)[2]
-        refList = hklGen(spaceGroup, cell, sMin, sMax, True, xtal=xtal)
+        #refList = hklGen(spaceGroup, cell, sMin, sMax, True, xtal=xtal)
         intensities = calcIntensity(refList, atomList, spaceGroup, wavelength)
         reflections = magRefList[:] + refList[:]
         intensities = np.append(magIntensities, intensities)
@@ -937,6 +937,7 @@ def diffPattern(infoFile=None, backgroundFile=None, wavelength=1.5403,
 # printInfo: prints out information about the provided space group and atoms,
 #   as well as the generated reflections
 def printInfo(cell, spaceGroup, atomLists, refLists, wavelength, symmetry=None):
+    print "Wavelength:", wavelength
     if (isinstance(refLists, ReflectionList)):
         atomLists = (atomLists,)
         refLists = (refLists,)

hklgen/hkl_model.py
@@ -209,12 +209,10 @@ class Model(object):
     def _set_reflections(self):
         maxLattice = self.cell.getMaxLattice()
         maxCell = CrystalCell(maxLattice[:3], maxLattice[3:])
-        self.refList = hklGen(self.spaceGroup, maxCell,
-                              self.sMin, self.sMax, True, xtal=self.xtal)
+        self.refList = hklGen(self.spaceGroup, self.cell.cell, self.sMin, self.sMax, True, xtal=self.xtal)
         self.reflections = self.refList[:]
         if self.magnetic:
-            self.magRefList = satelliteGen(self.cell.cell, self.symmetry,
-                                           self.sMax)
+            self.magRefList = satelliteGen(self.cell.cell, self.symmetry, self.sMax, hkls=self.refList)
             self.magReflections = self.magRefList[:]
 
     def __getstate__(self):
@@ -300,14 +298,13 @@ class Model(object):
         else:
             base, zero = self.base, self.zero
         plotPattern(self.peaks, self.background, self.tt-zero, self.observed,
-                            self.ttMin, self.ttMax, 0.01, self.exclusions, labels=None, base = base, residuals=True)          
+                            self.ttMin, self.ttMax, 0.01, self.exclusions, labels=None, base = base, residuals=False)          
 
 #    def _cache_cell_pars(self):
 #        self._cell_pars = dict((k,v.value) for k,v in self.cell.items())
     def update(self):
         self.cell.update()
         self.atomListModel.update()
-        
         hkls = [reflection.hkl() for reflection in self.reflections]
         sList = calcS(self.cell.cell, hkls)
         ttPos = np.array([twoTheta(s, self.wavelength) for s in sList])
@@ -315,8 +312,7 @@ class Model(object):
         ttPos[np.abs(ttPos - 180*np.ones_like(ttPos)) < 0.0001] = -20
         for i in xrange(len(self.reflections)):
             self.reflections[i].set_reflection_s(getS(ttPos[i], self.wavelength))
-        self.intensities = calcIntensity(self.refList, self.atomListModel.atomList, 
-                                         self.spaceGroup, self.wavelength)
+        self.intensities = calcIntensity(self.refList, self.atomListModel.atomList, self.spaceGroup, self.wavelength)
         self.peaks = makePeaks(self.reflections,
                                        [self.u.value, self.v.value, self.w.value],
                                        self.intensities, self.scale.value,
@@ -331,10 +327,7 @@ class Model(object):
             ttPos[np.abs(ttPos - 180*np.ones_like(ttPos)) < 0.0001] = -20
             for i in xrange(len(self.magReflections)):
                 self.magReflections[i].set_magh_s(getS(ttPos[i], self.wavelength))
-            #ind = 0
-            #for ref in self.magReflections:
-                #self.magRefList[ind] = ref
-                #ind += 1
+            printInfo(self.cell.cell, self.spaceGroup, [self.atomListModel.atomList, self.atomListModel.magAtomList], [self.refList,self.magRefList], self.wavelength, symmetry=self.newSymmetry)
             self.magIntensities = calcIntensity(self.magRefList,
                                                 self.atomListModel.magAtomList, 
                                                 self.newSymmetry, self.wavelength,